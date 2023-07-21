967f08fcc67dac661c680b34682925ab9e0e74ee
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-07-21 21:25:17 +0530

    Navbar expanded view is added.

--- components/Navbar/NavLink.js
+++ components/Navbar/NavLink.js
@@ -1,13 +1,25 @@
-import Link from "next/link"
+import { useSelector } from "react-redux"
 import { BsFillTriangleFill } from "react-icons/bs"
 
-const NavLink = ({ label, href, icon }) => {
-    return (<Link href={href} className='group hover:bg-slate-300 rounded p-2 my-2'>
+import Link from "next/link"
+
+const NavLink = ({ label, href, icon, onClick = () => { } }) => {
+    const { isExpanded } = useSelector(state => state.nav)
+    return (<Link href={href} className='w-min group flex hover:bg-slate-300 rounded p-2 my-2' onClick={onClick}>
         <span className='text-3xl text-gray-800'>{icon}</span>
-        <section className='absolute z-10 translate-x-12 -translate-y-8 hidden group-hover:flex items-center'>
-            <BsFillTriangleFill className='text-slate-200 -rotate-90 translate-x-1' />
-            <span className='bg-slate-200 text-gray-800 px-2 py-1 rounded'>{label}</span>
-        </section>
+        {label
+            ?
+            !isExpanded
+                ?
+                <section className='absolute z-10 translate-x-16 hidden group-hover:flex items-center'>
+                    <BsFillTriangleFill className='text-slate-200 -rotate-90 translate-x-1' />
+                    <span className='bg-slate-200 text-gray-800 px-2 py-1 rounded'>{label}</span>
+                </section>
+                :
+                <span className='font-bold text-gray-800 px-5 mx-5 rounded'>{label}</span>
+            :
+            null
+        }
     </Link>)
 }
 
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,13 +1,22 @@
+import { useRouter } from "next/router"
+import { useDispatch, useSelector } from "react-redux"
+import { navActions } from "@/store/nav-slice"
+
 import { IoLogoVercel } from "react-icons/io5"
-import { AiFillHome, AiFillSchedule, AiFillInfoCircle } from "react-icons/ai"
+import { AiFillHome, AiFillSchedule, AiFillInfoCircle, AiOutlineMenu } from "react-icons/ai"
 import { BsFillPersonFill, BsBarChartLineFill, BsFillChatDotsFill } from "react-icons/bs"
-import { FaSignOutAlt } from "react-icons/fa"
 
 import NavLink from "./NavLink"
 
 const Navbar = () => {
-    return (<nav className='h-screen flex flex-col justify-around bg-slate-200 p-3'>
-        <NavLink label={'Ease'} href={'/'} icon={<IoLogoVercel />} />
+    const { pathname } = useRouter()
+    const { isExpanded } = useSelector(state => state.nav)
+    const dispatch = useDispatch()
+    const navExpandHandler = () => {
+        dispatch(navActions.toggle())
+    }
+    return (<nav className={`h-screen flex flex-col justify-around bg-gradient-radial from-slate-200 to-slate-300 p-5`}>
+        <NavLink href={'/'} icon={<IoLogoVercel />} />
         <section className='flex flex-col'>
             <NavLink label={'Home'} href={'/'} icon={<AiFillHome />} />
             <NavLink label={'Plans'} href={'/plans'} icon={<AiFillSchedule />} />
@@ -16,7 +25,7 @@ const Navbar = () => {
             <NavLink label={'Contact'} href={'/contact'} icon={<BsFillChatDotsFill />} />
             <NavLink label={'Profile'} href={'/profile'} icon={<BsFillPersonFill />} />
         </section>
-        <NavLink label={'Sigout'} href={'/signin'} icon={<FaSignOutAlt />} />
+        <NavLink href={pathname} icon={<AiOutlineMenu />} onClick={navExpandHandler} />
     </nav>)
 }
 
